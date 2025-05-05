f0875046a810096773f5e59457d922b9d70d965e
Darkboy00 <you@example.com>
2025-05-05 21:57:41 +0300

    2DPROJECT

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public float jump;
     [SerializeField] private float speed = 5f;
     [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Animator animator;
@@ -13,11 +15,16 @@ public class PlayerMovement : MonoBehaviour
     {
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
         playerHalfWidth = spriteRenderer.bounds.extents.x;
+        rb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
         HandleMovement();
         FlipCharacter();
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(rb.linearVelocity.x, jump));
+        }
     }
     private void FlipCharacter()
     {
